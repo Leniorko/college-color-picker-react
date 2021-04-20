edbe875f36c733c152a3a038654d3a05a6954d34
Ivan Egorov <www.Len244@gmail.com>
2021-04-20 20:57:21 +0300

    Done with shitty code

--- index.js
+++ index.js
@@ -2,35 +2,88 @@ const fileInput = document.getElementById("person_picture")
 const imageCanvas = document.getElementById("imageCanvas")
 const imageCanvasContext = imageCanvas.getContext('2d')
 
-const firstColor = document.getElementsByClassName("color1")
-const secondColor = document.getElementsByClassName("color2")
-const thirdColor = document.getElementsByClassName("color3")
+const colorTxt = document.getElementsByClassName("color")
+const colorBgs = document.getElementsByClassName("color_bg")
 
-// function rgbToHex(color){
-//     return Number(color).toString(16).length == 1 
-//     ? "0" + Number(color).toString(16) 
-//     :    Number(color).toString(16)
-// }
+const predictedSpan = document.getElementsByClassName("predict")
+// Цвет кожи определяется как сумма R G B. Значения захардкожены
+// На основе цветов из skinSamples
+const blackSkinSample = 28 + 23 + 20
+const mulattoSkinSample = 173 + 120 + 94
+const whiteSkinSample = 245 + 218 + 207
+
+let clickCounter = 0
+let pickedColors = []
+
+function rgbToHex(color){
+    return Number(color).toString(16).length == 1 
+    ? "0" + Number(color).toString(16) 
+    :    Number(color).toString(16)
+}
+
+function colorPickingLogic(r, g, b){
+    let currentBlock = clickCounter % colorBgs.length
+    colorTxt[currentBlock].textContent = `#${rgbToHex(r)}${rgbToHex(g)}${rgbToHex(b)}`
+    colorBgs[currentBlock].style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+    pickedColors[currentBlock] = r + g + b
+
+    clickCounter += 1
+}
 
 let fileList = []
-let imageToCanvas = new Image
+let image = new Image
 
 fileInput.addEventListener("change", (event)=>{
     fileList = event.target.files
     console.log(fileList)
 
-    imageToCanvas.src = URL.createObjectURL(fileList[0])
+    image.src = URL.createObjectURL(fileList[0])
 })
 
-imageToCanvas.onload = function(e){
+image.onload = function(e){
     imageCanvas.setAttribute("width", this.width)
     imageCanvas.setAttribute("height", this.height)
-    imageCanvasContext.drawImage(imageToCanvas, 0, 0)
+    imageCanvasContext.drawImage(image, 0, 0)
 }
 
 imageCanvas.addEventListener("click", (e) =>{
     let rect = e.target.getBoundingClientRect()
     let pixel = imageCanvasContext.getImageData(e.clientX - rect.left, e.clientY - rect.top, 1, 1).data
-    console.log(pixel);
+
+    colorPickingLogic(pixel[0], pixel[1], pixel[2])
+
+    if (pickedColors.length == 3){
+        let b = 0
+        let w = 0
+        let m = 0
+
+        pickedColors.forEach(element => {
+            console.log(element);
+            if (element >= whiteSkinSample){
+                w += 1
+            } else if (element <= blackSkinSample){
+                b += 1
+            } else {
+                m += 1
+            }
+        })
+
+        let mostTo = Math.max(b, w, m)
+        console.log(mostTo);
+
+        if ([w, m, b].every(element => element == w)){
+            predictedSpan[0].textContent = "probably mulatto"
+        } else if (mostTo == w) {
+            predictedSpan[0].textContent = "white"
+        }else if (mostTo == b){
+            predictedSpan[0].textContent = "black"
+        } else {
+            predictedSpan[0].textContent = "mulatto"
+        }
+
+        console.log(w, m, b);
+    }
+
 })
 
